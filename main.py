3ab1301adb05e2210837f94feca2c6414ed6e9de
btbuilds <bthompson8807@gmail.com>
2025-06-18 10:00:47 -0400

    Refactor and began support for saving message log

--- main.py
+++ main.py
@@ -2,21 +2,39 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    if len(sys.argv) < 2:
+        print("No arguments provided.")
+        sys.exit(1)
 
-if len(sys.argv) < 2:
-    print("No arguments provided.")
-    sys.exit(1)
+    load_dotenv()
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=sys.argv[1]
-)
+    user_prompt = " ".join(sys.argv[1:])
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]    
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+
+    generate_response(client, messages)
+
+
+def generate_response(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
