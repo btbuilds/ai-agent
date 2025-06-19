4f70f8acb1af05d14d087aed21a25eeb249bc78a
btbuilds <bthompson8807@gmail.com>
2025-06-19 12:58:11 -0400

    Added directory checking

---
+++ functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+    working_dir = os.path.abspath(working_directory)
+    target_dir = working_dir
+
+    if directory:
+        target_dir = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not target_dir.startswith(working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    
+    try:
+        contents = []
+        for item in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, item)
+            contents.append(f"- {item}: file_size={int(os.path.getsize(filepath))} bytes, is_dir={os.path.isdir(filepath)}")
+        return "\n".join(contents)
+    except Exception as e:
+        return f"Error: {e}"
