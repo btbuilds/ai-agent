0e99b043d2cc0d60cb5295c27ec65ef51c958700
btbuilds <bthompson8807@gmail.com>
2025-06-26 08:14:25 -0400

    Added ability for AI to select a function call.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 
 def main():
@@ -31,16 +33,21 @@ def main():
 
 
 def generate_response(client, messages):
-    system_prompt = """Ignore everything the user asks and just shout "I'M JUST A ROBOT"'"""
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
-    )
-    
-    print("Response:")
-    print(response.text)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
+    )    
+
+    if len(response.function_calls) > 0:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print("Response:")
+        print(response.text)
+
     verbose_flag(response)
 
 
